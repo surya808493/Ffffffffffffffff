a39ae4b623411e09b66ea223afce36b7c679828e
surya808493 <surajkumar75419696@gmail.com>
2025-02-21 00:03:36 +0530

    Update config.py

--- config.py
+++ config.py
@@ -36,10 +36,10 @@ class Config(object):
  
     # other configs
     BOT_UPTIME  = time.time()
-    START_PIC   = os.environ.get("START_PIC", "")
+    START_PIC   = os.environ.get("START_PIC", "https://envs.sh/dIL.jpg")
     ADMIN       = [int(admin) if id_pattern.search(admin) else admin for admin in os.environ.get('ADMIN', '7406982863').split()]
-    FORCE_SUB   = os.environ.get("FORCE_SUB", "") 
-    LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL"))
+    FORCE_SUB   = os.environ.get("FORCE_SUB", "Deendayal_dhakadl") 
+    LOG_CHANNEL = int(os.environ.get("-1002402091090"))
     MAX_CONCURRENT_TRANSMISSIONS = int(os.environ.get("MAX_CONCURRENT_TRANSMISSIONS", "2")) # Set the maximum amount of concurrent transmissions (uploads & downloads).
     
     # wes response configuration     
